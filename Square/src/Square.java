eedb9cb5a3f3545995cf08bb9067d207862dbba1
dan1O1 <44612686+dan1O1@users.noreply.github.com>
2018-10-30 16:11:35 +0100

    Add files via upload

---
+++ Square/src/Square.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class Square {
+	
+
+
+		public static void main(String[] args) {
+			// Deklaration der Variablen
+			// Ganze Zahl, bis zu der multipliziert wird
+			int square;
+			// Ganze Zahl, die jeweils an der Reihe ist
+			int i;
+			// Festlegen der Startwerte der Variablen
+			square = 1;
+
+			Scanner sc = new Scanner(System.in);
+
+			System.out.println("Bitte eine Zahl eingeben:");
+			int number = sc.nextInt();
+			// Berechnen des Produktes product ueber alle Zahlen von 1 bis boundary
+
+			for (i = 1; i <= number; i++) {
+				 square = number* number;
+			}
+
+			// Ausgeben der Summe, gemeinsam mit erläuterndem Text
+			System.out.print("Das Quadrat von " + number + " durch " + number+" maliges Addieren von " + number);
+			System.out.print(" ist ");
+			System.out.println(square);
+		}
+	}
+
