ad2724800041c420e65c8a19df51a823dc81614e
dan1O1 <44612686+dan1O1@users.noreply.github.com>
2018-11-06 14:53:48 +0100

    Add files via upload

---
+++ PirateSpeechGenerator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PirateSpeechGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		boolean x = true;
+		while (x) {
+			System.out.println("Bitte geben Sie einen Text ein: ");
+
+			String eingabe = sc.nextLine();
+			if (eingabe.equals("Ende")) {
+				sc.close();
+				x = false;
+			} else {
+
+				eingabe = eingabe.replace("a", "arr");
+				eingabe = eingabe.replace("e", "err");
+				eingabe = eingabe.replace("o", "orr");
+				eingabe = eingabe.replace("u", "urr");
+				eingabe = eingabe.replace("i", "irr");
+			}
+
+			System.out.println(eingabe);
+		}
+
+	}
+
+}
